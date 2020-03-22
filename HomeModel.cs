e8aae7000d79437300f964b8bc26beb1b6046ac2
Georglucas <40433282+Georglucas@users.noreply.github.com>
2020-03-22 00:49:19 -0300

    Add files via upload

---
+++ HomeModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCOREMVC.Models
+{
+    public class HomeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<HomeModel> GetAll()
+        {
+            HomeModel item;
+            List<HomeModel> lista = new List<HomeModel>();
+
+            item = new HomeModel();
+            item.Id = 1;
+            item.Name = "Georg Lc.";
+            lista.Add(item);
+
+            item = new HomeModel();
+            item.Id = 2;
+            item.Name = "Junior Analyst";
+            lista.Add(item);
+
+            return lista;
+        }
+    }
+}
